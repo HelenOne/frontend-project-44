af7300e92b44005bbc0d42cf0126ae2b6492fb44
Elena Gromova <elena.gromova@hexlet.io>
2024-07-09 15:25:09 +0300

    changed dirs structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,45 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 
-const roundsCount = 3;
+import runEvenGame from '../src/games/even.js';
 
-const welcomeMessage = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  return name;
-};
-
-const isEven = (number) => {
-  return number % 2 === 0;
-};
-
-const playRound = () => {
-  const number = Math.floor(Math.random() * 100) + 1;
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ').trim().toLowerCase();
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  return { answer, correctAnswer };
-};
-
-const main = () => {
-  const name = welcomeMessage();
-  let correctAnswersCount = 0;
-
-  while (correctAnswersCount < roundsCount) {
-    const { answer, correctAnswer } = playRound();
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-}
-
-main()
+runEvenGame();
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const roundsCount = 3;
